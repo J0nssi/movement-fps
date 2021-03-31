5c608a92ce64acf257a8b264de114eb7aaee6038
Lassi Köykkä <lassi_koykka@hotmail.com>
2021-03-31 12:43:21 +0300

    Lisäsin animaation rigit ja ensimmäisen persoonan modelit.

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -24,7 +24,7 @@ public class MouseLook : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -90f, 75f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
 
